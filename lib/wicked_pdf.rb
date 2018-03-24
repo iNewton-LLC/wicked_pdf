1ab3c7135d96c0b15c3597faeb86a406dabac221
Erik Axel Nielsen <erikaxel@lucalabs.com>
2018-03-24 18:03:10 +0100

    Support for using headless chrome instead of wkhtmltopdf.

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -37,10 +37,12 @@ class WickedPdf
   attr_accessor :binary_version
 
   def initialize(wkhtmltopdf_binary_path = nil)
-    @exe_path = wkhtmltopdf_binary_path || find_wkhtmltopdf_binary_path
-    raise "Location of #{EXE_NAME} unknown" if @exe_path.empty?
-    raise "Bad #{EXE_NAME}'s path: #{@exe_path}" unless File.exist?(@exe_path)
-    raise "#{EXE_NAME} is not executable" unless File.executable?(@exe_path)
+    unless WickedPdf.config[:use_puppeteer]
+      @exe_path = wkhtmltopdf_binary_path || find_wkhtmltopdf_binary_path
+      raise "Location of #{EXE_NAME} unknown" if @exe_path.empty?
+      raise "Bad #{EXE_NAME}'s path: #{@exe_path}" unless File.exist?(@exe_path)
+      raise "#{EXE_NAME} is not executable" unless File.executable?(@exe_path)
+    end
 
     retrieve_binary_version
   end
@@ -67,8 +69,14 @@ class WickedPdf
     # merge in global config options
     options.merge!(WickedPdf.config) { |_key, option, _config| option }
     generated_pdf_file = WickedPdfTempfile.new('wicked_pdf_generated_file.pdf', options[:temp_path])
-    command = [@exe_path]
-    command << '-q' unless on_windows? # suppress errors on stdout
+    if WickedPdf.config[:use_puppeteer]
+      spec = Gem::Specification.find_by_name('wicked_pdf')
+      node_modules_path = "#{Rails.root.to_s}/node_modules"
+      command = ['node', File.join(spec.gem_dir, 'lib', 'wicked_pdf', 'pdf.js'), node_modules_path]
+    else
+      command = [@exe_path]
+      command << '-q' unless on_windows? # suppress errors on stdout
+    end
     command += parse_options(options)
     command << url
     command << generated_pdf_file.path.to_s
